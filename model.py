4741b3a29a13c48ccaef026bc1462a067e69fbd6
logicalelegance <chris@logicalelegance.com>
2019-11-19 16:27:08 -0800

    Final model

--- model.py
+++ model.py
@@ -16,10 +16,12 @@ import sklearn
 from math import ceil
 from random import shuffle
 from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers import Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers import MaxPooling2D
+from keras.layers import BatchNormalization
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -33,20 +35,21 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 name = './IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_RGB2BGR)
                 center_angle = float(batch_sample[3])
-           #     center_image_flipped = np.fliplr(center_image)
-           #     center_angle_flipped = -center_angle
+                center_image_flipped = np.fliplr(center_image)
+                center_angle_flipped = -center_angle
                 images.append(center_image)
                 angles.append(center_angle)
-           #     images.append(center_image_flipped)
-           #     angles.append(center_angle_flipped)
+                images.append(center_image_flipped)
+                angles.append(center_angle_flipped)
 
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # Set our batch size, sized to use up available memory on laptop
-batch_size=512
+batch_size=128
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=batch_size)
@@ -55,30 +58,31 @@ validation_generator = generator(validation_samples, batch_size=batch_size)
 model = Sequential()
 
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1.,
+model.add(Lambda(lambda x: x/255 - 0.5,
         input_shape=(160, 320, 3),
         output_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((65,25), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Convolution2D(24,(5,5), subsample=(2,2), activation='relu'))
 model.add(Convolution2D(36,(5,5), subsample=(2,2),  activation='relu'))
 model.add(Convolution2D(48,(5,5), subsample=(2,2), activation='relu'))
 model.add(Convolution2D(64,(3,3), activation='relu'))
 model.add(Convolution2D(64,(3,3), activation='relu'))
 model.add(Flatten())
+model.add(Dropout(0.2))
 model.add(Dense(100))
 model.add(Dense(50))
-model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-
-# step size is *2 to account for augmentation
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+filepath="weights.best.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
 model.fit_generator(train_generator,
-            steps_per_epoch=ceil(len(train_samples)/batch_size), 
+            steps_per_epoch=ceil(2*len(train_samples)/batch_size), 
             validation_data=validation_generator,
-            validation_steps=ceil(len(validation_samples)/batch_size),
-            epochs=5, verbose=1, use_multiprocessing=True)
+            validation_steps=ceil(2*len(validation_samples)/batch_size),
+            epochs=5, verbose=1, use_multiprocessing=True, callbacks=callbacks_list)
 
 model.save('model.h5')
 exit()
